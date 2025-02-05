4aafb8d139466c7f7d8929b9df4d875444fb7074
Tolper4161 <arseniy.z2006@gmail.com>
2025-02-05 18:52:23 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const TEXT_AREA_MAX_LINE_COUNT = 5.5;
+
+function chatInputAutoResizeTA(textarea) {
+    const countOfLines = textarea.value.split('\n').length;
+
+    const lineHeight = parseFloat(textarea.style.lineHeight);
+    const minHeight = parseFloat(textarea.style.minHeight);
+    const maxHeight = minHeight + lineHeight * TEXT_AREA_MAX_LINE_COUNT;
+    
+    const height = minHeight + lineHeight * (countOfLines - 1);
+    
+    if (height <= maxHeight) {
+        textarea.style.height = height + 'px';
+    }
+    else {
+        textarea.style.height = maxHeight + 'px';
+    }
+}
+
+function chatSendBtnUpdateText() {
+    const chatSendBtn = document.querySelector('.send-btn');
+    if (window.innerWidth <= 768) {
+        chatSendBtn.innerHTML = '<img src="https://img.icons8.com/?size=24&id=87599&format=png&color=ffffff" alt="send-msg-icon">';
+    }
+    else {
+        chatSendBtn.innerHTML = 'Отправить';
+    }
+}
+
+window.addEventListener('load', chatSendBtnUpdateText);
+window.addEventListener('resize', chatSendBtnUpdateText);
