6efbe134b484d266dacbe3a5d4edaf291cd81325
Tolper4161 <arseniy.z2006@gmail.com>
2025-02-06 16:40:53 +0300

    Mobile optimization

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const TEXT_AREA_MAX_LINE_COUNT = 5.5;
+const TEXT_AREA_MAX_LINE_COUNT = 4.5;
 
 function chatInputAutoResizeTA(textarea) {
     const countOfLines = textarea.value.split('\n').length;
 
     const lineHeight = parseFloat(textarea.style.lineHeight);
     const minHeight = parseFloat(textarea.style.minHeight);
-    const maxHeight = minHeight + lineHeight * TEXT_AREA_MAX_LINE_COUNT;
+    const maxHeight = minHeight + lineHeight * (TEXT_AREA_MAX_LINE_COUNT - 1.5);
     
     const height = minHeight + lineHeight * (countOfLines - 1);
     
@@ -17,15 +17,15 @@ function chatInputAutoResizeTA(textarea) {
     }
 }
 
-function chatSendBtnUpdateText() {
-    const chatSendBtn = document.querySelector('.send-btn');
-    if (window.innerWidth <= 768) {
-        chatSendBtn.innerHTML = '<img src="https://img.icons8.com/?size=24&id=87599&format=png&color=ffffff" alt="send-msg-icon">';
-    }
-    else {
-        chatSendBtn.innerHTML = 'Отправить';
-    }
-}
+// function chatSendBtnUpdateText() {
+//     const chatSendBtn = document.querySelector('.send-btn');
+//     if (window.innerWidth <= 768) {
+//         chatSendBtn.innerHTML = '<img src="https://img.icons8.com/?size=24&id=87599&format=png&color=ffffff" alt="send-msg-icon">';
+//     }
+//     else {
+//         chatSendBtn.innerHTML = 'Отправить';
+//     }
+// }
 
-window.addEventListener('load', chatSendBtnUpdateText);
-window.addEventListener('resize', chatSendBtnUpdateText);
+// window.addEventListener('load', chatSendBtnUpdateText);
+// window.addEventListener('resize', chatSendBtnUpdateText);
